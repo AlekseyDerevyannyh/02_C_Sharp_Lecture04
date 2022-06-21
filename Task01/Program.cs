df2cbfd5b85878749f6998309896e6b764f4d1f2
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-06-21 14:08:50 +0300

    add Task 1 Fill & Print matrix

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Заполняем двумерную матрицу числами и выводим её на экран
+using System;
+using static System.Console;
+
+Clear();
+int[,] matrix = new int[3,4];
+PrintArray(matrix);
+FillArray(matrix);
+WriteLine();
+PrintArray(matrix);
+
+void PrintArray(int[,] matr) {
+	for (int i = 0; i < matr.GetLength(0); i ++) {
+		for (int j = 0; j < matr.GetLength(1); j ++) {
+			Write($"{matr[i, j]} ");
+		}
+		WriteLine();
+	}
+}
+
+void FillArray (int[,] matr) {
+	for (int i = 0; i < matr.GetLength(0); i ++) {
+		for (int j = 0; j < matr.GetLength(1); j ++) {
+			matr[i, j] = new Random().Next(1, 10);
+		}
+	}
+}
