8d4d79792acbd1faaf7ab59a033e4d489bf1f2b0
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-06-21 15:39:18 +0300

    add Task 3 Factorial

---
+++ Task03_Factorial/Program.cs
@@ -0,0 +1,12 @@
+﻿// Факториал числа через рекурсию
+using System;
+using static System.Console;
+
+Clear();
+
+Write($"{Factorial(5)}");
+
+int Factorial (int n) {
+	if (n == 1) 	return 1;
+	else			return n * Factorial(n - 1);
+}
